a431342ecfcc7fcfec99661a63cc4ff304b53bfb
Ketan Vaghela <ketanvaghela2230@gmail.com>
2021-03-12 18:50:26 +0530

    FireBase Task

--- final/src/environments/environment.ts
+++ final/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDx804VdFLKc0Ku3sz9v898XRte_Ot7Zys",
+    authDomain: "angular9crud-5f474.firebaseapp.com",
+    projectId: "angular9crud-5f474",
+    storageBucket: "angular9crud-5f474.appspot.com",
+    messagingSenderId: "294913025197",
+    appId: "1:294913025197:web:35edc9a911e093c0429224",
+    measurementId: "G-79N37YVTPK"
+  }
 };
 
 /*
