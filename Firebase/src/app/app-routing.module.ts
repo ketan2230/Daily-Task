9766f4378c5299939eceabbbd559f9e458179578
Ketan Vaghela <ketanvaghela2230@gmail.com>
2021-03-16 17:35:46 +0530

    Email Password Authentication

--- Firebase/src/app/app-routing.module.ts
+++ Firebase/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
+import { RealTimeDatabaseComponent } from '../app/real-time-database/real-time-database.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// Required services for navigation
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Import all the components for which navigation service has to be activated 
+import { SignInComponent } from '../app/components/sign-in/sign-in.component';
+import { SignUpComponent } from '../app/components/sign-up/sign-up.component';
+import { DashboardComponent } from '../app/components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from '../app/components/forgot-password/forgot-password.component';
+import { AuthGuard } from "../app/shared/guard/auth.guard";
+import { VerifyEmailComponent } from '../app/components/verify-email/verify-email.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full'},
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register-user', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'realtime', component: RealTimeDatabaseComponent },
+  { path: 'verify-email-address', component: VerifyEmailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- Firebase/src/app/app.module.ts
+++ Firebase/src/app/app.module.ts
@@ -4,20 +4,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { FormsModule } from '@angular/forms';  
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';  
 import { AngularFireModule } from '@angular/fire';  
 import { AngularFireDatabaseModule } from '@angular/fire/database';  
 import { AngularFirestoreModule } from '@angular/fire/firestore';  
-
 import { RealTimeDatabaseComponent } from './real-time-database/real-time-database.component';  
-import { CloudFirestoreComponent } from './cloud-firestore/cloud-firestore.component'; 
+import { CloudFirestoreComponent } from './cloud-firestore/cloud-firestore.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component'; 
+import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     RealTimeDatabaseComponent,  
-    CloudFirestoreComponent
+    CloudFirestoreComponent, DashboardComponent, SignInComponent, SignUpComponent, ForgotPasswordComponent, VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +34,7 @@ import { CloudFirestoreComponent } from './cloud-firestore/cloud-firestore.compo
     AngularFirestoreModule  ,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [AngularFireModule],
+  providers: [AngularFireModule, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
