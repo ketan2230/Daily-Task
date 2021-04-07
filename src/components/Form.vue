97ad0067ef2366d7170237a360bee00dbdbdfae3
Ketan Vaghela <ketanvaghela2230@gmail.com>
2021-04-07 18:00:42 +0530

    Watcher

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -125,28 +125,4 @@ div {
 label {
     display: flex;
 }
-label {
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
-}
-input + label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-input[type="text"],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
 </style>
